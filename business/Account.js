ae179e354382dbc9f19417c94a29ddda93726376
ZXL <zhouxiuliang@suishipen.com>
2016-05-30 17:24:26 +0800

    init

---
+++ business/Account.js
@@ -0,0 +1,21 @@
+import model from '../models';
+import returnNext from '../lib/common/returnNext';
+import {formatCreateData} from '../lib/common/sequelizeUtil';
+
+export default class User {
+	async getAccount() {
+		let accounts = await model.account.findAll({
+			include: [{
+				model: model.user
+			}]
+		});
+
+		return returnNext(null, 200, accounts);
+	}
+
+	async addAccount(data) {
+		let account = await model.account.create(data);
+
+		return returnNext(null, 200, account);
+	}
+}
